bbe2c2accf55530d0e49841e50118182b24e38a4
Brandon Cooke <brandoncookedev@gmail.com>
2019-03-24 12:48:06 -0400

    Fixed batch files to read image name file accurately

--- docker/scripts/windows/run.bat
+++ docker/scripts/windows/run.bat
@@ -3,7 +3,9 @@ SET BASEDIR=%~dp0
 REM SET IMAGE_NAME=image_name
 
 REM Set the Image Name from the text file content
-for /f %%i in ("type %BASEDIR%\..\ImageName.txt") do set IMAGE_NAME=%%i
+for /f %%i in ('type %BASEDIR%\..\ImageName.txt') do set IMAGE_NAME=%%i
+
+echo Running Docker container %IMAGE_NAME%
 
 cd %BASEDIR%\..\..\..
 docker run -it %IMAGE_NAME%
